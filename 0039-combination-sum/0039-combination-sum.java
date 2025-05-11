8c353feeb95478d24bb08178760b815f047c5011
prudhvi <112791386+prudhvi-dot@users.noreply.github.com>
2025-05-11 08:46:53 +0530

    Time: 2 ms (82.99%), Space: 44.9 MB (31.8%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,24 +2,27 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
 
-        generate(candidates, 0, target, new ArrayList<Integer>(), res);
+        generate(candidates, 0, target, 0, new ArrayList<Integer>(), res);
 
         return res;
     }
 
-    void generate(int[] nums, int i, int target, List<Integer> list, List<List<Integer>> res) {
-        if (target < 0) return;
+    void generate(int[] nums, int i, int target, int sum, List<Integer> list, List<List<Integer>> res) {
+
+        if(sum>target) {
+            return;
+        }
         if(i==nums.length) {
-            if(0==target){
+            if(sum==target){
              res.add(new ArrayList<Integer>(list));   
             }   
             return;
         }
 
         list.add(nums[i]);
-        generate(nums, i, target-nums[i], list, res);
+        generate(nums, i, target, sum+nums[i], list, res);
         list.remove(list.size()-1);
-        generate(nums, i+1, target, list, res);
+        generate(nums, i+1, target, sum, list, res);
 
     }
 }
