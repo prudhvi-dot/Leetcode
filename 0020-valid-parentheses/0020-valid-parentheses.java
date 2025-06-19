bb3f5390536449bebccab2995f94c0cf952fc395
prudhvi <prudhvisai3772@gmail.com>
2025-06-19 20:22:41 +0530

    Time: 2 ms (97.04%), Space: 41.7 MB (65.56%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,19 +4,16 @@ class Solution {
 
         for(int i = 0; i<s.length(); i++) {
             char ch = s.charAt(i);
-            if(ch=='(' || ch=='[' || ch=='{') {
+
+            if(ch == '(' || ch== '{' || ch=='[') {
                 stack.push(ch);
-            }
-            else {
+            }else {
                 if(stack.isEmpty()) {
                     return false;
                 }
-
-                char top = stack.peek();
-
-                if(top=='(' && ch==')' || top=='{' && ch=='}' || top=='[' && ch==']') {
+                else if(stack.peek()=='[' && ch==']' || stack.peek()=='(' && ch==')' || stack.peek()=='{' && ch=='}'){
                     stack.pop();
-                }else{
+                }else {
                     return false;
                 }
             }
