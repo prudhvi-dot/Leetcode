417552beea93470fd19aca1d053fb5e32daa261c
prudhvi <prudhvisai3772@gmail.com>
2025-06-24 14:58:29 +0530

    Time: 14 ms (57.58%), Space: 49.4 MB (7.64%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -6,7 +6,7 @@ class Solution {
             map.put(n,map.getOrDefault(n,0)+1);
         }
 
-        PriorityQueue<Map.Entry<Integer,Integer>> maxHeap = new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
+        PriorityQueue<Map.Entry<Integer,Integer>> maxHeap = new PriorityQueue<>((a,b)->Integer.compare(b.getValue(),a.getValue()));
 
         for(Map.Entry<Integer,Integer> entry: map.entrySet()) {
             maxHeap.add(entry);
